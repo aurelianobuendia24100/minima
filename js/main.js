5c695842aee00461397c8f7ad3d542f6d5583b16
aureliano buendia <aureliano@buendia.com>
2020-11-10 16:55:57 +0200

    initial

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  var $button = $('#button'),
+    $name = $('#name'),
+    $address = $('#address'),
+    $number = $('#number'),
+    $form = $('form')
+  $form.submit(function(e) {
+    e.preventDefault();
+
+  });
+});
