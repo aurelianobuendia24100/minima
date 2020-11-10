9d00921b852150fe4a78c2d4b5ae5b63ab4ab1ff
aureliano buendia <aureliano@buendia.com>
2020-11-10 19:44:51 +0200

    v1

--- js/main.js
+++ js/main.js
@@ -5,18 +5,61 @@ $(document).ready(function(){
     $formData = $('form#data'),
     $conversation = $('#conversation');
 
+  if (localStorage.getItem('name'))
+    $name.val(localStorage.getItem('name'))
+
+  if (localStorage.getItem('address'))
+    $address.val(localStorage.getItem('address'))
+
+  if (localStorage.getItem('number'))
+    $number.val(localStorage.getItem('number'))
+
   $formData.submit(function(e) {
     e.preventDefault();
+
+    localStorage.setItem('name', $name.val());
+    localStorage.setItem('address', $address.val());
+    localStorage.setItem('number', $number.val());
+
     var message = $number.val() + ' ' + $name.val() + ' ' + $address.val(),
         $yourMessage = $('#yourMessage'),
       $yourDate = $('#yourDate'),
-      $theirMessage = $('#theirMessage'),
-        $theirDate = $('#theirDate');
+      $theirMessage = $('#theirMessage');
+    $yourDate.text(getDate());
     $yourMessage.text(message);
-    $theirMessage.text('Μετακίνηση ' + message);
+    $theirMessage.text('ΜΕΤΑΚΙΝΙΣΗ ' + normalizeGreek(message));
     $conversation.show();
 
   });
+
+  function getDate() {
+    var d = new Date();
+
+    d.setMinutes(d.getMinutes() - 15); // timestamp
+    d = new Date(d);
+
+    minutes = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
+
+    var clock = d.getHours() + ":" + minutes;
+    var month = d.getMonth() + 1;
+    var day = d.getDate();
+    var currentDate =
+        (("" + day).length < 2 ? "0" : "") +
+        day +
+        "." +
+        (("" + month).length < 2 ? "0" : "") +
+        month +
+        "." +
+        d.getFullYear() +
+        "  " +
+        clock;
+
+    return currentDate;
+  }
+
+  function normalizeGreek(text) {
+    return text.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+  }
 });
 var elem = document.getElementById("body");
 
